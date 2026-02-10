4b609e6d17cecd67f39ebc032eb50adb8231ccb4
Saad7890-web <omysaadislam@gmail.com>
2026-02-10 18:56:49 +0600

    added health status in service

--- internal/registry/registry.go
+++ internal/registry/registry.go
@@ -3,29 +3,60 @@ package registry
 import (
 	"net/url"
 	"sync"
+	"sync/atomic"
+)
+
+type Status int32
+
+const (
+	Healthy Status = iota
+	Unhealthy
 )
 
 type Service struct {
-	ID string
-	URL *url.URL
+	ID     string
+	URL    *url.URL
+	status atomic.Int32
+}
+
+func (s *Service) SetStatus(st Status) {
+	s.status.Store(int32(st))
+}
+
+func (s *Service) Status() Status {
+	return Status(s.status.Load())
 }
 
 type Registry struct {
-	mu sync.RWMutex
+	mu       sync.RWMutex
 	services []*Service
 }
 
-func New() *Registry{
+func New() *Registry {
 	return &Registry{}
 }
 
-func(r *Registry)Add(service *Service){
+func (r *Registry) Add(service *Service) {
+	service.SetStatus(Healthy)
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	r.services = append(r.services, service)
 }
 
-func (r *Registry) List() []*Service {
+func (r *Registry) HealthyServices() []*Service {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	var healthy []*Service
+	for _, s := range r.services {
+		if s.Status() == Healthy {
+			healthy = append(healthy, s)
+		}
+	}
+	return healthy
+}
+
+func (r *Registry) All() []*Service {
 	r.mu.RLock()
 	defer r.mu.RUnlock()
 	return r.services
